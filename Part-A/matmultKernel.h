6dd22209a30936c381af61d7ac1fca7ad78685e0
Ben Cyna <benjamincyna@gmail.com>
2025-11-22 15:25:06 -0500

    updates to c

--- Part-A/matmultKernel.h
+++ Part-A/matmultKernel.h
@@ -15,28 +15,31 @@
 
 // Defines for size of thread block and data computed by a thread block
 #define BLOCK_SIZE 16
-
-// FOOTPRINT_SIZE can be overridden at compile time (e.g. -DFOOTPRINT_SIZE=32)
 #ifndef FOOTPRINT_SIZE
 #define FOOTPRINT_SIZE BLOCK_SIZE
 #endif
 
 // The type Matrix is really a MATRIX DESCRIPTOR. 
 // Matrices are stored in row major order:
-//   M[row,col] = *(M.elements + row * M.stride + col)
+//       M[row,col] = *(M.elements + row * M.stride + col)
 //
-// A submatrix is not copied but represented as a descriptor that
-// points into a larger matrix. The 'stride' is the width (in elements)
-// from one row of the large matrix to the next row.
+// A sub matrix is not copied but allocated in the full matrix.
+//
+// This requires the stride of the full matrix to properly get to the
+// next row of the sub matrix (a block).
+//
+// Stride is the width in bytes from one element of the larger matrix 
+// to the element in the same column but one row down.
+
 
 typedef struct {
-    int   width;
-    int   height;
-    int   stride;
-    float *elements;
+  int width;
+  int height;
+  int stride;
+  float* elements;
 } Matrix;
 
 // Forward declaration of the kernel function that performs the work.
-__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C);
+__global__ void MatMulKernel(const Matrix, const Matrix, Matrix);
 
-#endif // __MMKERNEL__
+#endif
